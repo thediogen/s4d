d4a1cdad75e438e91d0ed10e6bdfeef49e535fe9
thediogen <evgendvodnenko@gmail.com>
2025-04-21 17:54:42 +0300

    data write lesson

--- shop/market/urls.py
+++ shop/market/urls.py
@@ -1,12 +1,16 @@
-# from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-# from shop import market
-from .views import index, about_us
-
+from .views import (index, get_person_by_pk, get_person_by_surname, change_surname, get_or_create_person,
+                    update_or_create_person, create_person, delete_person, get_all_persons)
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('aboutus', about_us, name='aboutus'),
-    # path('register', register, name='register')
+    path("", index, name="index"),
+    path("get/<int:pk>", get_person_by_pk),
+    path("get/<str:surname>", get_person_by_surname),
+    path("get_or_create_person/<int:pk>", get_or_create_person),
+    path("change_surname/<int:pk>/<str:surname>", change_surname),
+    path("update/<str:name>", update_or_create_person),
+    path("create", create_person),
+    path("delete/<int:pk>", delete_person),
+    path("all", get_all_persons)
 ]
--- shop/market/views.py
+++ shop/market/views.py
@@ -1,38 +1,105 @@
+import traceback
+
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
-
+from .models import Person
 
 def index(request):
 
     context = {
-        'title': 'GoIteens Shop'
+        "title": "TEST TITLE"
     }
 
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html', context)
 
 
-def about_us(request):
-    context = {
-        'title': 'About Us'
-    }
+def get_person_by_pk(request, pk: int):
+    try:
+        person = Person.objects.get(pk=pk)
+        return HttpResponse(f"{person}")
+    except ObjectDoesNotExist:
+        return HttpResponse(f"error: Person with {pk} does not exists")
 
-    return render(request, 'aboutUs.html', context=context)
+def get_person_by_surname(request, surname: str):
+    person = Person.objects.get(last_name=surname)
+    return HttpResponse(f"{person}")
 
+
+def get_or_create_person(request, pk:int):
+    person = Person.objects.get_or_create(
+        pk=pk,
+        defaults={
+            "first_name": "John",
+            "last_name": "Sina"
+        }
+    )
+
+    return HttpResponse(f"{person}")
+
+
+def change_surname(request, pk: int, surname: str):
+    try:
+        person = Person.objects.get(pk=pk)
+
+        person.last_name = surname
+        person.save()
+
+        return HttpResponse(f"New surname for user - {person.first_name}, {person.last_name}")
+
+    except ObjectDoesNotExist:
+        return HttpResponseBadRequest(
+            {
+                "error": f"Person with {pk} does not exists",
+                "traceback": traceback.format_exc()
+            }
+        )
+
+
+def create_person(request):
+    p = Person(first_name="Patrick", last_name="Saint")
+
+    p.save()
+    return HttpResponse("Created")
+
+
+def update_or_create_person(request, name:str):
+    person = Person.objects.update_or_create(
+        first_name=name,
+        defaults={
+            "first_name": "Oguzik",
+            "last_name": "Lavrov"
+        }
+    )
+    return HttpResponse(f"{person}")
+
+
+def delete_person(request, pk: int):
+    person = Person.objects.get(
+        pk=pk,
+    )
+    person.delete()
+    return HttpResponse(f"Deleted person")
+
+def get_all_persons(request):
+    persons = Person.objects.all()
+    for person in persons:
+        print(person)
+
+    return render(request, "persons.html", context={"persons": persons})
 
 # def register(request):
 #     if request.method == "POST":
 #         form = NewUserForm(request.POST)
-
+#
 #         if form.is_valid():
 #             user = form.save()
 #             login(request, user)
 #             messages.success(request, "Registration successful")
-#             return redirect('index')
+#             return redirect("index")
 #         else:
 #             return render(request, '400.html')
-
+#
 #     form = NewUserForm()
 #     return render(request=request, template_name="register.html", context={"register_form": form})
